8620a99908cee2b1810d9511c4f15f667e93f8b8
DuyQK <duyqk@runsystem.net>
2018-01-01 22:31:42 +0700

    init

---
+++ CustomRenderer/CustomRendererPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CustomRenderer
+{
+    public partial class CustomRendererPage : ContentPage
+    {
+        public CustomRendererPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
